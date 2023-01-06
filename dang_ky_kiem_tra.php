1817e655295630a5eea48e374a5ccb9c27be0460
ThaoNe1101 <121376928+ThaoNe1101@users.noreply.github.com>
2023-01-06 16:07:53 +0700

    Add files via upload

--- dang_ky_kiem_tra.php
+++ dang_ky_kiem_tra.php
@@ -2,13 +2,13 @@
     // Kết nối với cơ sở dữ liệu
     include 'config.php';
     // Gán 3 biến POST được gửi đến vào 3 biến của mình
-    $email = $_POST["txtEmail"];
+    $kh_email = $_POST["txtEmail"];
     $mat_khau = $_POST["txtMatKhau"];
-    $ho_ten = $_POST["txtHoTen"];
-    $sdt = $_POST["txtSdt"];
+    $kh_ho_ten = $_POST["txtHoTen"];
+    $kh_dien_thoai = $_POST["txtSdt"];
 
     //Câu lệnh truy vấn kiểm tra sự trùng lặp tài khoản không
-    $sql = "SELECT * FROM `tbl_khach_hang` where `kh_email` ='" .$email. "'";
+    $sql = "SELECT * FROM tbl_khach_hang WHERE kh_email=\"".$email."\"";
     // Thực hiện câu lênh
     $result = $conn->query($sql);
 
@@ -21,8 +21,8 @@
             </script>
         ";
     } else { // Tài khoản mới không tồn tại
-            $sql="INSERT INTO `tbl_khach_hang`( `kh_ho_ten`, `kh_dia_chi`, `kh_dien_thoai`, `kh_email`, `kh_cccd`, `kh_ghi_chu`) 
-            VALUES ('" . $ho_ten . "','" . $dia_chi . "','" . $dien_thoai . "','" . $email . "','" . $cccd . "','" . $ghi_chu . "')";
+            $sql="INSERT INTO `tbl_khach_hang`(`kh_ho_ten`, `kh_dien_thoai`, `kh_email`, `mat_khau`) 
+            VALUES ('$kh_ho_ten','$kh_dien_thoai','$kh_email','$mat_khau')";
             if($conn->query($sql)===TRUE) {
                 echo "
                     <script type='text/javascript'>
